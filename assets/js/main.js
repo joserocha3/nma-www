fd7f48f67eb63c031758bd17126933b73e18ac68
Pablo Rocha <jose.rocha3@gmail.com>
2017-09-16 16:50:03 -0500

    change email to formspring

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,15 +15,3 @@ function closeMenu() {
 }
 
 var scroll = new SmoothScroll('a[href*="#"]');
-
-function sendEmail() {
-    Email.send(
-        "test@test.com",
-        "pablo@nomoreanalog.com",
-        "This is a subject",
-        "this is the body",
-        "smtp.gmail.com",
-        "pablo@nomoreanalog.com",
-        "helpmenow101");
-    // {token: "3de63837-50ce-43bf-9492-d34d9397f02c"});
-}
